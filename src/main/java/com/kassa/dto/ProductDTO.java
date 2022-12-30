56ddcd1f10a389cd2890f593ed017994360bc5cb
Alevtina Bazarnova <abazarnova@alee.ru>
2022-12-30 12:56:39 +0300

    added lombok

--- src/main/java/com/kassa/dto/ProductDTO.java
+++ src/main/java/com/kassa/dto/ProductDTO.java
@@ -1,7 +1,14 @@
 package com.kassa.dto;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDTO {
     private Long id;
     private String productName;
@@ -10,132 +17,4 @@ public class ProductDTO {
     private BigDecimal weight;
     private Long checkId;
     private String description;
-
-    public ProductDTO(Builder builder) {
-        this.id = builder.id;
-        this.productName = builder.name;
-        this.account = builder.account;
-        this.amount = builder.amount;
-        this.weight = builder.weight;
-        this.checkId = builder.checkId;
-        this.description = builder.description;
-    }
-
-    public ProductDTO() {
-    }
-
-    public ProductDTO(Long id, String productName, Integer amount, Integer account, BigDecimal weight, Long checkId, String description) {
-        this.id = id;
-        this.productName = productName;
-        this.amount = amount;
-        this.account = account;
-        this.weight = weight;
-        this.checkId = checkId;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    public Integer getAccount() {
-        return account;
-    }
-
-    public void setAccount(Integer account) {
-        this.account = account;
-    }
-
-    public BigDecimal getWeight() {
-        return weight;
-    }
-
-    public void setWeight(BigDecimal weight) {
-        this.weight = weight;
-    }
-
-    public Long getCheckId() {
-        return checkId;
-    }
-
-    public void setCheckId(Long checkId) {
-        this.checkId = checkId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public static class Builder {
-        Long id;
-        String name;
-        Integer amount;
-        Integer account;
-        BigDecimal weight;
-        Long checkId;
-        String description;
-
-        public Builder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder setAmount(Integer amount) {
-            this.amount = amount;
-            return this;
-        }
-
-        public Builder setAccount(Integer account) {
-            this.account = account;
-            return this;
-        }
-
-        public Builder setWeight(BigDecimal weight) {
-            this.weight = weight;
-            return this;
-        }
-
-        public Builder setCheckId(Long checkId) {
-            this.checkId = checkId;
-            return this;
-        }
-
-        public Builder setDescription(String description) {
-            this.description = description;
-            return this;
-        }
-
-        public ProductDTO build() {
-            return new ProductDTO(this);
-        }
-    }
 }
